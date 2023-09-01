8fa5c4388795c80788d99e0c773bd7f051ee18c2
miguez <miguezroberto@gmail.com>
2023-09-01 18:52:34 +0100

    fix: chunks are larger than 500 kBs after minification Fixes #26

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,21 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
